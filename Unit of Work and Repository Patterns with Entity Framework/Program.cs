03632e06dee8f541a8a194988df40bbdf3eb72de
Austin Kaylor <austinjohnkaylor@outlook.com>
2022-05-18 22:23:47 -0400

    using explicit types everywhere in file. Also registering the DbContext ApplicationDbContext in Program.cs

--- Unit of Work and Repository Patterns with Entity Framework/Program.cs
+++ Unit of Work and Repository Patterns with Entity Framework/Program.cs
@@ -1,4 +1,7 @@
-var builder = WebApplication.CreateBuilder(args);
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -7,7 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
