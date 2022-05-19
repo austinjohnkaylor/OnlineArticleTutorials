9c80540953977a23233f8966765c5ed6cb2ba267
Austin Kaylor <austinjohnkaylor@outlook.com>
2022-05-18 22:39:47 -0400

    Registering the Users model as a DbSet in the ApplicationDbContext.cs

--- Unit of Work and Repository Patterns with Entity Framework/Data/ApplicationDbContext.cs
+++ Unit of Work and Repository Patterns with Entity Framework/Data/ApplicationDbContext.cs
@@ -1,11 +1,21 @@
+using API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 public class ApplicationDbContext : DbContext
 {
+    // The DbSet property will tell EF Core tha we have a table that needs to be created
+    public virtual DbSet<User> Users { get; set; }
+    
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+    
+    // On model creating function will provide us with the ability to manage the tables properties
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
     }
 }
